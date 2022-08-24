43638a94ee763d2e759aab12cde2ba8db484950a
Imran <imrantipu90@yahoo.com>
2022-08-24 09:17:20 +0200

    Mileston 6 : Check number,string,boolean and validation

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,18 @@
 // -----------------------Milestone 6------------------------------------
 // 
 //  -----calculating date-----------------
-const date1 = new Date('1971-03-26');
-const date2 = new Date('1971-12-16');
-//  comparing date and time in millisecond
-if(date1.getTime < date2.getTime); 
+// const date1 = new Date('1971-03-26');
+// const date2 = new Date('1971-12-16');
+// //  comparing date and time in millisecond
+// if(date1.getTime < date2.getTime); 
+
+// -----------data type check
+
+const number = 25;
+if(typeof number === 'number'){
+    console.log('number');
+
+}
+else{
+    console.log('not a number');
+}
