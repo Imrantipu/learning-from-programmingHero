0ad3a40b07fdd92167c42ed8d66b12b9ce2be61c
Imran <imrantipu90@yahoo.com>
2022-08-24 08:08:20 +0200

    try and catch error understode

---
+++ javaScript.js
@@ -0,0 +1,13 @@
+// -----------------------Milestone 6------------------------------------
+try{
+    console.log('inside try');
+}
+
+catch(error){
+    console.log('inside catch');
+    console.log(error);
+}
+
+finally{
+    console.log('last line');
+}
