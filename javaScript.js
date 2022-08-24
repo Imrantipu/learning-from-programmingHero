a9e81182a5073458497915e07deeee7ac040ca41
Imran <imrantipu90@yahoo.com>
2022-08-24 08:32:33 +0200

    learned about date object

--- javaScript.js
+++ javaScript.js
@@ -1,13 +1,7 @@
 // -----------------------Milestone 6------------------------------------
-try{
-    console.log('inside try');
-}
-
-catch(error){
-    console.log('inside catch');
-    console.log(error);
-}
-
-finally{
-    console.log('last line');
-}
+// 
+//  -----calculating date-----------------
+const date1 = new Date('1971-03-26');
+const date2 = new Date('1971-12-16');
+//  comparing date and time in millisecond
+if(date1.getTime < date2.getTime); 
